bdb19325fbefde74e0c6df4841399e32c13a1e79
konishis <gts.0424.koni2@gmail.com>
2021-02-18 15:02:21 +0900

    pylint success

---
+++ exercise_python_koni2/json2xml.py
@@ -0,0 +1,67 @@
+"""
+○使用外部パッケージ
+    poetry add BeautifulSoup4
+    poetry add lxml
+    poetry add dicttoxml
+○参考
+    jsonからxmlについて
+        https://qiita.com/kkdmgs110/items/9317057facb60764ed74
+    BeautifulSoupやパーサについて
+        https://qiita.com/PND/items/06e1053eeed69ec4f418
+        https://lets-hack.tech/programming/languages/python/beautifulsoup-xml/
+○内容
+    指定したjsonファイルを読み込み、xmlに変換する。その後パース(見やすく)し、ファイル出力する。
+    ここでは「jsonfile.json」を読み込み、「xmlfile.xml」を出力している。
+"""
+
+import json
+from bs4 import BeautifulSoup
+import dicttoxml
+
+
+def json2xml():
+    """json形式をxml形式に変換する"""
+    translatedjsonfile = jsontodictfile(readjsonfile())
+    translatedxmlfile = dicttoxmlfile(translatedjsonfile)
+    writexmlfile(shapingxmlfile(translatedxmlfile))
+    print("jsonからxmlを作成完了")
+
+
+def readjsonfile():
+    """指定したjsonを読み込んで返す"""
+    targetfile = open(r"readfiles\jsonfile.json", "r")
+    return targetfile
+
+
+def writexmlfile(writefile):
+    """指定したxmlに書き込んで返す"""
+    # print("書き込み中")
+    with open(r"xmlfile.xml", "wt") as xmlfile:
+        xmlfile.write(writefile)
+        return xmlfile
+
+
+def shapingxmlfile(xmlfile):
+    """外部モジュール'BeautifulSoup4'を使用し、xmlを整形する"""
+    # print("xmlFileを整形中")
+    shapingfile = BeautifulSoup(xmlfile, "lxml")
+    return shapingfile.prettify()
+
+
+def jsontodictfile(targetfile):
+    """jsonをdictに変換して返す"""
+    # print("jsonから辞書に変換中")
+    translateddictfile = json.load(targetfile)
+    return translateddictfile
+
+
+def dicttoxmlfile(targetfile):
+    """dictをxmlに変換して返す"""
+    # print("辞書からxmlに変換中")
+    translatedxmlfile = dicttoxml.dicttoxml(targetfile)
+    # print(translatedxmlfile)
+    return translatedxmlfile
+
+
+if __name__ == "__main__":
+    json2xml()
---
+++ tests/test_exercise_python_koni2.py
@@ -0,0 +1,5 @@
+from exercise_python_koni2 import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
